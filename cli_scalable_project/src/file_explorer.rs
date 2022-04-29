b3cfb6937f41f418f3f73c93ab12d4d291011df8
rzanchetta01 <rzanchetta01@gmail.com>
2022-04-29 15:40:14 -0300

    test in file_explorer implemented

--- cli_scalable_project/src/file_explorer.rs
+++ cli_scalable_project/src/file_explorer.rs
@@ -1,4 +1,4 @@
-pub fn read_text(path : String) -> Result<String, Box<dyn std::error::Error>> {
+pub fn read_text(path: String) -> Result<String, Box<dyn std::error::Error>> {
     let contents: String = std::fs::read_to_string(path)?;
     return Ok(contents);
 }
@@ -23,4 +23,75 @@ pub fn search_in_text(content: &str, querry: &Vec<String>) -> Result<Vec<String>
     }
 
     return Ok(result);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn read_valid_text() {
+        let filepath: String = "T:\\PROEJTOS\\My-Rep-Rust\\cli_reader_app\\poem.pdf"
+            .trim()
+            .to_string();
+
+        match read_text(filepath) {
+            Ok(_) => println!(""),
+            Err(_) => panic!(""),
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn read_invalid_text() {
+        let filepath: String = "T:\\PROEJTOS\\My-Rep-Rust\\cli_reader_app\\poem.pdfs"
+            .trim()
+            .to_string();
+
+        match read_text(filepath) {
+            Ok(_) => println!(""),
+            Err(_) => panic!(""),
+        }
+    }
+
+    #[test]
+    fn search_valid_in_text() {
+        let filepath: String = "T:\\PROEJTOS\\My-Rep-Rust\\cli_reader_app\\poem.pdf"
+            .trim()
+            .to_string();
+
+        let querry: Vec<String> = ["uol".trim().to_string()].to_vec();
+        let text: String;
+
+        match read_text(filepath) {
+            Ok(s) => text = s,
+            Err(_) => panic!(""),
+        }
+
+        match search_in_text(&text, &querry) {
+            Ok(_) => println!(""),
+            Err(_) => panic!(""),
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn search_invalid_in_text() {
+        let filepath: String = "T:\\PROEJTOS\\My-Rep-Rust\\cli_reader_app\\poem.pdfs"
+            .trim()
+            .to_string();
+        let querry: Vec<String> = ["uol".trim().to_string()].to_vec();
+        let text: String;
+
+        match read_text(filepath) {
+            Ok(s) => text = s,
+            Err(_) => panic!(""),
+        }
+
+        match search_in_text(&text, &querry) {
+            Ok(_) => println!(""),
+            Err(_) => panic!(""),
+        }
+    }
 }
